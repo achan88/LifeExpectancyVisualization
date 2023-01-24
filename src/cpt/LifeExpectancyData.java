5ab7074aac29a4229c221a2352eafbc7a2e62a6e
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2023-01-23 23:49:07 -0500

    Comments

--- src/cpt/LifeExpectancyData.java
+++ src/cpt/LifeExpectancyData.java
@@ -1,16 +1,32 @@
 package cpt;
 
 public class LifeExpectancyData {
+
+    // instance variables
     private String country;
     private int year;
     private double lifeExpectancy;
 
+    /**
+     * Constructor for LifeExpectancyData class
+     * 
+     * @param country country of data
+     * @param year year of data 
+     * @param lifeExpectancy life expectancy data 
+     * @author A. Chan
+     */
     public LifeExpectancyData(String country, int year, double lifeExpectancy) {
         this.country = country;
         this.year = year;
         this.lifeExpectancy = lifeExpectancy;
     }
 
+    /**
+     * Getter method for country variable
+     * 
+     * @return country
+     * @author A. Chan
+     */
     public String getCountry() {
         return country;
     }
